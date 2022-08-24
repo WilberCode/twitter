d8d8a983b7b9fa7178c99988550eb5fb58ec27ea
wilber <parionahuaraca@gmail.com>
2022-08-24 02:10:47 -0500

    cambios

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,7 @@ export default NextAuth({
   providers: [
     TwitterProvider({
         clientId: process.env.TWITTER_CLIENT_ID,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET,
-        secret: process.env.NEXT_PUBLIC_SECRET,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET, 
         version:'2.0'
      })
     // ...add more providers here
